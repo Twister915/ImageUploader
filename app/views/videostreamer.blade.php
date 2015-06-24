2eca60ef321a4471329504b1d0670290bafd8609
Joey Sacchini <joey.sacchini264@gmail.com>
2015-06-23 23:47:08 -0400

    Test

--- app/views/videostreamer.blade.php
+++ app/views/videostreamer.blade.php
@@ -6,7 +6,7 @@
   </head>
   <body>
     <video class="video-js vjs-default-skin"
-  controls preload="auto" width="640" height="264"
+  controls preload="auto" width="100%" height="100%"
   poster="http://video-js.zencoder.com/oceans-clip.png">
    <source src="http://tmg.pw/raw/{{{$videoId}}}" type='{{{$videoMime}}}' />
    <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
